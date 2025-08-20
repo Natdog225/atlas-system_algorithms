d3b78a0dedc1b8f9b522855216bc07fb7d5be368
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-08-20 17:53:05 -0500

    This one almost seems too simple. Will adjust once I put it through the checker.

---
+++ nary_trees/1-nary_tree_delete.c
@@ -0,0 +1,24 @@
+#include "nary_trees.h"
+
+/**
+ * nary_tree_delete - Deallocates an entire N-ary tree.
+ * @tree: Pointer to the root of the tree to delete.
+ */
+void nary_tree_delete(nary_tree_t *tree)
+{
+	nary_tree_t *child, *next_child;
+
+	if (!tree)
+		return;
+
+	child = tree->children;
+	while (child)
+	{
+		next_child = child->next;
+		nary_tree_delete(child);
+		child = next_child;
+	}
+
+	free(tree->content);
+	free(tree);
+}
